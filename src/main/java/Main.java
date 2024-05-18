33447804e821c23adc9105390c1e962194b84a9f
maark1106 <mark1106@naver.com>
2024-05-19 01:41:31 +0900

    feat : MYSQL 연동

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
-package org.example;
+import databaseUtils.Admin;
+import databaseUtils.User;
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection admin = Admin.getAdminConnection();
+            Connection user = User.getUserConnection();
+            System.out.println("DB 연결완료");
+        }catch (ClassNotFoundException e){
+            System.out.println("JDBC 드라이버 로드 오류");
+        } catch (RuntimeException e) {
+            System.out.println("DB 연결 오류");
+        }
     }
 }
